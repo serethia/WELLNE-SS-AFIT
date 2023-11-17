87fecf0801f5e2fd3176dcd81294c2e92e67936b
Sewon Oum <sewon0306@naver.com>
2023-11-17 09:29:16 +0900

    New branch test

--- Front/src/views/CategoryView.vue
+++ Front/src/views/CategoryView.vue
@@ -1,30 +1,25 @@
 <template>
     <div>
-        <h2>카테고리명마다 바꿔주기~~</h2>
-        <p>카테고리 뷰:{{ routeName }}</p>
+        <h2>{{ routeName }}</h2>
     </div>
 </template>
 
 <script setup>
-import {useRoute} from 'vue-router';
+import { useRoute } from 'vue-router';
 import { onMounted, watch, ref } from 'vue';
 
 const route = useRoute();
 const routeName = ref('')
 
+// 뷰에 표시할 routeName에 카테고리 반영
 onMounted(()=>{
-    console.log(route.params.category)
     routeName.value = route.params.category
 })
 
+// 카테고리 nav 누를 때마다 지속적으로 변화 감지하도록 watch 작성
 watch(route, (newVal)=>{
-    console.log(newVal.params.category);
     routeName.value = newVal.params.category
-
 })
-
-
-
 </script>
 
 <style scoped>
